f5c9896686c7a6ca9834c41532f64ef5c25b635b
Manpreet055 <singhmanpreet57577@gmail.com>
2026-01-14 00:08:24 +0400

    feat: add smooth animation to HeroSection text and buttons

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -18,10 +18,10 @@ const NavbarComponent = () => {
         </span>
       </NavbarBrand>
       <div className="flex items-center relative md:order-2">
-        <span className="absolute right-10 md:right-25 top-1/2 -translate-y-1/2">
+        <span className="absolute right-10 md:right-28 top-1/2 -translate-y-1/2">
           <DarkThemeToggle />
         </span>
-        <button className="dark:text-black dark:bg-white bg-black text-white py-2 font-semibold hover:bg-gray-200 transition-all duration-300 ease-in-out px-5 rounded-4xl  hidden md:block">
+        <button className="dark:text-black my-2 dark:bg-white bg-black text-white py-2 font-semibold hover:bg-gray-200 transition-all duration-300 ease-in-out px-5 rounded-4xl  hidden md:block">
           Hire Me
         </button>
         <NavbarToggle />
--- src/sections/HeroSection.jsx
+++ src/sections/HeroSection.jsx
@@ -1,29 +1,52 @@
 import React from "react";
 import Blob from "../ui/Blob";
 import TypewriterComponent from "typewriter-effect";
+import { motion } from "framer-motion";
 
 const HeroSection = () => {
   return (
     <div className="mx-3 sm:mx-6 mt-2 sm:mt-6 overflow-hidden rounded-3xl relative flex items-start min-h-150vh px-4 sm:px-8 xl:px-32 py-8 sm:py-16 bg-gray-100 dark:bg-gray-800">
       <div className="p-1 sm:p-4 max-w-2xl gap-6 h-full flex flex-col items-start justify-center">
-        <h3 className="">Mern Stack Developer</h3>
-        <h1 className="text-4xl sm:text-6xl font-bold max-w-2xs sm:max-w-lg">
+        <motion.h3
+          initial={{ opacity: 0, x: -100 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 0.3 }}
+          className="font-semibold text-lg"
+        >
+          Mern Stack Developer
+        </motion.h3>
+        <motion.h1
+          initial={{ opacity: 0, x: -100 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 0.5 }}
+          className="text-4xl sm:text-6xl font-bold max-w-2xs sm:max-w-lg"
+        >
           Hello, I'm Manpreet Singh
-        </h1>
-        <p className="text-sm sm:text-base   tracking-wide leading-snug sm:leading-relaxed sm:max-w-lg ">
+        </motion.h1>
+        <motion.p
+          initial={{ opacity: 0, x: -100 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 0.7 }}
+          className="text-sm sm:text-base   tracking-wide leading-snug sm:leading-relaxed sm:max-w-lg "
+        >
           I’m a full-stack MERN developer who builds and deploys modern,
           scalable web applications. I focus on clean UI, solid backend
           architecture, and real-world features using React, Node.js, Express,
           and MongoDB.
-        </p>
-        <div className="flex items-center gap-4">
-          <button className=" px-5 py-3 primary-bg-gradient text-white rounded-3xl text-sm font-semibold hover:bg-blue-700 shadow-2xl transition">
+        </motion.p>
+        <motion.div
+          initial={{ opacity: 0, x: -100 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 0.9 }}
+          className="flex items-center gap-4"
+        >
+          <button className="px-5 py-3 primary-bg-gradient text-white rounded-3xl text-sm font-semibold hover:bg-blue-700 shadow-2xl transition">
             Contact Me
           </button>
           <button className="px-5 py-3 bg-white text-black rounded-3xl text-sm font-semibold hover:bg-gray-200 shadow-2xl transition-all duration-300 ease-in-out">
             View Portfolio
           </button>
-        </div>
+        </motion.div>
       </div>{" "}
       {/* <Blob
         position="bottom-90 left-100 "
