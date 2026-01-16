564b00dd6962b034c8ec98bd7898353f1ebec84c
Manpreet055 <singhmanpreet57577@gmail.com>
2026-01-16 22:40:23 +0400

    feat : Banner component and Tech Stack Section images

---
+++ src/utils/techStackImgs.js
@@ -0,0 +1,45 @@
+import html from "../assets/html.webp";
+import css from "../assets/css.webp";
+import js from "../assets/javascript.webp";
+import react from "../assets/react.webp";
+import tailwind from "../assets/tailwind.webp";
+import RHF from "../assets/RHF.webp";
+import router from "../assets/router.webp";
+import nodejs from "../assets/nodejs.webp";
+import jwt from "../assets/jwt.webp";
+import express from "../assets/express.webp";
+import git from "../assets/git.webp";
+import github from "../assets/github.webp";
+import motion from "../assets/framer-motion.webp";
+import ubuntu from "../assets/ubuntu.webp";
+import flowbite from "../assets/flowbite.webp";
+import cloudinary from "../assets/cloudinary.webp";
+import axios from "../assets/axios.webp";
+import bcrypt from "../assets/bcrypt.webp";
+import mongodb from "../assets/mongodb.webp";
+import multer from "../assets/multer.webp";
+
+const images = [
+  html,
+  css,
+  js,
+  react,
+  tailwind,
+  RHF,
+  router,
+  nodejs,
+  jwt,
+  express,
+  git,
+  github,
+  motion,
+  ubuntu,
+  flowbite,
+  cloudinary,
+  axios,
+  bcrypt,
+  mongodb,
+  multer,
+];
+
+export default images;
