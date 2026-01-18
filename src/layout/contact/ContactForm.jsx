fbeef2e2b0394b7d22bbc0475202b61518c9f9ce
Manpreet055 <singhmanpreet57577@gmail.com>
2026-01-18 22:06:47 +0400

    refactor : Enhanced Contact Form and Details Layout fix : responsive text sizing in Contact Form

--- src/layout/contact/ContactForm.jsx
+++ src/layout/contact/ContactForm.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import { set, useForm } from "react-hook-form";
+import { useForm } from "react-hook-form";
 import { motion } from "framer-motion";
 import sendMessage from "../../services/contactService.js";
 import { Spinner } from "flowbite-react";
-
+import { User, Mail } from "lucide-react";
 const ContactForm = () => {
   const [loading, setLoading] = React.useState(false);
   const [error, setError] = React.useState(null);
@@ -31,62 +31,70 @@ const ContactForm = () => {
     >
       <form
         onSubmit={handleSubmit(onSubmit)}
-        className="flex flex-col gap-4 w-full py-6 px-4 max-w-xl mx-auto"
+        className="flex flex-col gap-3 sm:gap-4 w-full p-6  max-w-xl mx-auto"
       >
-        <div className="flex flex-col gap-2">
+        <div className="flex flex-col gap-1">
           <label
             htmlFor="name"
-            className="text-gray-400 dark:text-gray-300 font-medium"
+            className="text-gray-400 text-sm sm:text-base dark:text-gray-300 font-medium"
           >
             Name
           </label>
-          <input
-            id="name"
-            placeholder="Name"
-            {...register("name", { required: "Name is required" })}
-            className="dark:border  shadow-md  dark:border-gray-500 p-2 rounded bg-gray-100 dark:bg-gray-700"
-          />
+          <span className="relative">
+            <User className="absolute left-2 top-1/2 transform -translate-y-1/2 text-gray-400 " />
+            <input
+              id="name"
+              placeholder="Name"
+              {...register("name", { required: "Name is required" })}
+              className="dark:border w-full text-sm sm:text-base  shadow-md  dark:border-gray-500 p-2 pl-10 rounded bg-gray-100 dark:bg-gray-700"
+            />
+          </span>
           {errors.name && (
             <span className="text-red-500 text-sm">{errors.name.message}</span>
           )}
         </div>
-        <div className="flex flex-col gap-2">
+        <div className="flex flex-col gap-1">
           <label
             htmlFor="email"
-            className="text-gray-400 dark:text-gray-300 font-medium"
+            className="text-gray-400 text-sm sm:text-base dark:text-gray-300 font-medium"
           >
             Email
           </label>
-          <input
-            id="email"
-            type="email"
-            placeholder="Your Email"
-            {...register("email", {
-              required: "Email is required",
-              pattern: {
-                value: /^\S+@\S+$/i,
-                message: "Invalid email address",
-              },
-            })}
-            className="dark:border  shadow-md dark:border-gray-500 p-2 rounded"
-          />
+          <span className="relative">
+            <Mail className="absolute left-2 top-1/2 transform -translate-y-1/2 text-gray-400 " />
+            <input
+              id="email"
+              type="email"
+              placeholder="Your Email"
+              {...register("email", {
+                required: "Email is required",
+                pattern: {
+                  value: /^\S+@\S+$/i,
+                  message: "Invalid email address",
+                },
+              })}
+              className="dark:border pl-10 w-full text-sm sm:text-base  shadow-md dark:border-gray-500 p-2 rounded bg-gray-100 dark:bg-gray-700"
+            />
+          </span>
           {errors.email && (
             <span className="text-red-500 text-sm">{errors.email.message}</span>
           )}
         </div>
+
+        {/* Subject */}
         <select
           {...register("subject", { required: "Subject is required" })}
-          className="dark:border  shadow-md dark:border-gray-500 p-2 rounded"
+          className="dark:border mt-4 text-sm sm:text-base shadow-md dark:border-gray-500 p-2 rounded bg-gray-100 dark:bg-gray-700"
         >
           <option value="">Select a subject</option>
           <option value="general">General Inquiry</option>
           <option value="support">Support</option>
           <option value="feedback">Feedback</option>
         </select>
-        <div className="flex flex-col gap-2">
+        <div className="flex flex-col gap-1">
           <label
             htmlFor="message"
-            className="text-gray-400 dark:text-gray-300 font-medium"
+            className="text-gray-400 text-sm sm:text-base dark:text-gray-300 font-medium"
           >
             Message
           </label>
@@ -94,7 +102,7 @@ const ContactForm = () => {
             placeholder="Your Message"
             id="message"
             {...register("message", { required: "Message is required" })}
-            className="dark:border  shadow-md dark:border-gray-500 p-2 rounded h-32"
+            className="dark:border  shadow-md dark:border-gray-500 p-2 rounded h-32 text-sm sm:text-base bg-gray-100 dark:bg-gray-700"
           />
           {errors.message && (
             <span className="text-red-500 text-sm">
@@ -106,7 +114,7 @@ const ContactForm = () => {
           <button
             disabled={loading}
             type="submit"
-            className="primary-bg-gradient rounded-3xl font-medium px-10 py-3 text-white transitions hover:scale-105"
+            className="primary-bg-gradient rounded-3xl mt-2 font-medium px-10 p-3 text-sm sm:text-base text-white transitions hover:scale-105"
           >
             {loading ? (
               <span className="flex items-center gap-2">
